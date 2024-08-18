58d768d21a9d48a47d5925ffec9e0dfbcafcdd63
Nathan G <thailgrott@gmail.com>
2024-08-18 00:32:16 -0500

    Added /Plugin.Activate and /VolumeDriver.Capabilities routes, functionality

--- app/main.go
+++ app/main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+    "encoding/json"
     "log"
     "net/http"
     "os"
+
     "github.com/gorilla/mux"
 )
 
+// PluginActivateResponse represents the response to the /Plugin.Activate request
+type PluginActivateResponse struct {
+    Implements []string `json:"Implements"`
+}
+
+// VolumeDriverCapabilitiesResponse represents the response to the /VolumeDriver.Capabilities request
+type VolumeDriverCapabilitiesResponse struct {
+    Capabilities struct {
+        Scope string `json:"Scope"`
+    } `json:"Capabilities"`
+}
+
 func main() {
-    log.SetOutput(os.Stdout)
-    log.SetFlags(log.LstdFlags | log.Lshortfile)
+    r := mux.NewRouter()
 
-    btrfsManager := NewBtrfsManager("/var/lib/docker-snap-volumes")
-    pluginAPI := NewPluginAPI(btrfsManager)
+    // Handle the /Plugin.Activate route
+    r.HandleFunc("/Plugin.Activate", func(w http.ResponseWriter, r *http.Request) {
+        response := PluginActivateResponse{
+            Implements: []string{"VolumeDriver"},
+        }
+        json.NewEncoder(w).Encode(response)
+    }).Methods("POST")
 
-    router := mux.NewRouter()
-    router.HandleFunc("/VolumeDriver.Create", pluginAPI.CreateVolume).Methods("POST")
-    router.HandleFunc("/VolumeDriver.Remove", pluginAPI.RemoveVolume).Methods("POST")
-    router.HandleFunc("/VolumeDriver.Mount", pluginAPI.MountVolume).Methods("POST")
-    router.HandleFunc("/VolumeDriver.Unmount", pluginAPI.UnmountVolume).Methods("POST")
+    // Handle the /VolumeDriver.Capabilities route
+    r.HandleFunc("/VolumeDriver.Capabilities", func(w http.ResponseWriter, r *http.Request) {
+        response := VolumeDriverCapabilitiesResponse{}
+        response.Capabilities.Scope = "local"
+        json.NewEncoder(w).Encode(response)
+    }).Methods("POST")
 
-    log.Println("Starting HTTP server on :8080")
-    if err := http.ListenAndServe(":8080", router); err != nil {
-        log.Fatalf("Failed to start plugin API: %v", err)
+    // Handle routes for the standard Docker volume plugin functionality
+    r.HandleFunc("/VolumeDriver.Create", CreateVolume).Methods("POST")
+    r.HandleFunc("/VolumeDriver.Remove", RemoveVolume).Methods("POST")
+    r.HandleFunc("/VolumeDriver.Mount", MountVolume).Methods("POST")
+    r.HandleFunc("/VolumeDriver.Unmount", UnmountVolume).Methods("POST")
+
+    // Start the HTTP server
+    log.Println("Starting the SnapVol Docker Volume Plugin")
+    err := http.ListenAndServe("/run/docker/plugins/snapvol.sock", r)
+    if err != nil {
+        log.Fatalf("Error starting server: %v", err)
+        os.Exit(1)
     }
 }
