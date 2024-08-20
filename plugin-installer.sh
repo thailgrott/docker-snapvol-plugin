6fb9390b5f3ef718a3538d7c96ac0b4b9ff58776
Nathan G <thailgrott@gmail.com>
2024-08-19 23:39:09 -0500

    fix plugin installation

--- plugin-installer.sh
+++ plugin-installer.sh
@@ -9,10 +9,14 @@ docker plugin disable snapvol-plugin:latest
 
 mkdir -p ${basepath}
 
+docker stop temp_snapvol
+docker rm temp_snapvol
+
 docker run --rm -it --name temp_snapvol -v ${basepath}:/mnt snapvol /bin/sh \
 -c "cd / && tar czf /mnt/snapvol-plugin.tar.gz bin sbin usr/bin usr/sbin lib usr/lib etc config.json"
 
-tar -xzf ${basepath}/snapvol-plugin.tar.gz -C ${basepath}
+tar -xzf ${basepath}/snapvol-plugin.tar.gz -C ${basepath} config.json
+tar -xzf ${basepath}/snapvol-plugin.tar.gz -C ${basepath}/rootfs/ .
 
 # Make sure the socket directory exists and is writable by the Docker daemon.
 mkdir -p /run/docker/plugins
